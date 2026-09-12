0f5d69c105b653f6c0e1e4fd6f3f62282c7b0680
tranluongphukiet-ops <tranluongphukiet@gmail.com>
2026-09-12 02:56:49 +0000

    Lab 1: Hello Dart

--- hello_dart/bin/hello_dart.dart
+++ hello_dart/bin/hello_dart.dart
@@ -1,7 +1,7 @@
 import 'package:hello_dart/hello_dart.dart' as hello_dart;
 
 void main() {
- var ten = 'Sinh vien';
+ var ten = 'Sinh vien HCE';
  print('Xin chao, $ten! Day la chuong trinh Dart dau tien.');
  for (var i = 1; i <= 5; i++) {
  print('Dem: $i');
