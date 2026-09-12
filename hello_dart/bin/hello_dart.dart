fd8f49449027065dd52832fbb73c41b78f06a5da
tranluongphukiet-ops <tranluongphukiet@gmail.com>
2026-09-12 01:48:59 +0000

    dong bo 12/9

---
+++ hello_dart/bin/hello_dart.dart
@@ -0,0 +1,10 @@
+import 'package:hello_dart/hello_dart.dart' as hello_dart;
+
+void main() {
+ var ten = 'Sinh vien';
+ print('Xin chao, $ten! Day la chuong trinh Dart dau tien.');
+ for (var i = 1; i <= 5; i++) {
+ print('Dem: $i');
+ }
+}
+
